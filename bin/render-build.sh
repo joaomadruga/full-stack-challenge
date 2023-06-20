6e9f917d5a6a934bc89752f66e9b9386a4d4a58e
Joao Madruga <joaop050702@hotmail.com>
2023-06-20 11:31:29 -0300

    Adding render deploy configs List of changes: - Created render-build.sh file - Updated config files for accepting enviroment variables from render

---
+++ bin/render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rake db:migrate
+bundle exec rake db:seed
+bundle exec rails tailwindcss:install
